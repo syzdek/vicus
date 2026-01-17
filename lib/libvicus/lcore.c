a6df4cca8ed7b074dcf04f08c7044598e97c7688
David M. Syzdek <david@syzdek.net>
2026-01-16 23:01:32 -0900

    libvicus: adding stub

---
+++ lib/libvicus/lcore.c
@@ -0,0 +1,113 @@
+/*
+ *  Versatile IKE Control User Software for strongSwan
+ *  Copyright (C) 2026 David M. Syzdek <david@syzdek.net>.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are
+ *  met:
+ *
+ *     1. Redistributions of source code must retain the above copyright
+ *        notice, this list of conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright
+ *        notice, this list of conditions and the following disclaimer in the
+ *        documentation and/or other materials provided with the distribution.
+ *
+ *     3. Neither the name of the copyright holder nor the names of its
+ *        contributors may be used to endorse or promote products derived from
+ *        this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ *  IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ *  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ *  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ *  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ *  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ *  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ *  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#define __LIB_LIBVICUS_LCORE_C 1
+#include "libvicus.h"
+
+
+///////////////
+//           //
+//  Headers  //
+//           //
+///////////////
+// MARK: - Headers
+
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <unistd.h>
+
+
+//////////////
+//          //
+//  Macros  //
+//          //
+//////////////
+// MARK: - Macros
+
+
+///////////////////
+//               //
+//  Definitions  //
+//               //
+///////////////////
+// MARK: - Definitions
+
+
+//////////////////
+//              //
+//  Prototypes  //
+//              //
+//////////////////
+// MARK: - Prototypes
+
+
+/////////////////
+//             //
+//  Functions  //
+//             //
+/////////////////
+// MARK: - Functions
+
+//int
+//vicus_connect(
+//         vicus_t *                     vd );
+
+
+int
+vicus_disconnect(
+         vicus_t *                     vd )
+{
+   if (!(vd))
+      return(0);
+
+   if (vd->s == -1)
+      close(vd->s);
+
+   return(0);
+}
+
+
+//int
+//vicus_init_fd(
+//         vicus_t **                    vdp,
+//         int                           fd,
+//         int                           proto );
+
+
+//int
+//vicus_initialize(
+//         vicus_t **                    vdp,
+//         const char *                  uri );
+
+/* end of source */
